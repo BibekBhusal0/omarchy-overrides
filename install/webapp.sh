4c420142aa186db0de0668c07ef5ff65c3815cbf
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-01 11:18:29 +0545

    feat: install webapps

---
+++ install/webapp.sh
@@ -0,0 +1,5 @@
+omarchy-webapp-install "Chess" https://chess.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chess.png
+omarchy-webapp-install "Gemini (AI Studio)" https://aistudio.google.com/prompts/new_chat?model=gemini-3-pro-preview https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/gemini.png
+omarchy-webapp-install "Podcast" https://pocketcasts.com/podcasts https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/pocket-casts.png
+omarchy-webapp-install "Gemini (AI Studio)" https://aistudio.google.com/prompts/new_chat?model=gemini-3-pro-preview https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/gemini.png
+omarchy-webapp-install "Reddit" https://reddit.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/reddit.png
