f3747f6a0a3f00e773a7f32fe8f99a638a1b0f14
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-06 10:18:56 +0545

    also install open code and node

--- install/others.sh
+++ install/others.sh
@@ -3,3 +3,5 @@
 yay -Sy --noconfirm --needed brave-bin
 yay -Sy --noconfirm --needed yazi
 yay -Sy --noconfirm --needed legcord-bin
+yay -Sy --noconfirm --needed opencode-bin
+omarchy-install-dev-env node
