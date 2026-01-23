ca3f8b144d7dc40b10c6255e7e582b0d9b98c2c4
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-23 19:17:48 +0545

    feat: ✨ alias for git commands

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -77,3 +77,12 @@ gce () {
   emojified_text=$(devmoji --text "$1" | sed 's/"/\\"/g')
   git commit -am "$emojified_text"
 }
+
+git config --global alias.a 'add'
+git config --global alias.ps 'push'
+git config --global alias.pl 'pull'
+git config --global alias.c 'commit -m'
+git config --global alias.s 'status'
+git config --global alias.co 'checkout'
+git config --global alias.b 'branch'
+git config --global alias.l "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --decorate --date=short"
