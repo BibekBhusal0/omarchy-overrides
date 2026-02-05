be29d24bb0f7513dca887acb81ee998798780e12
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-05 16:31:45 +0545

    fix: 🐛 don't run p10k instant prompt if using tmux

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -1,8 +1,11 @@
 # If not running interactively, don't do anything (leave this at the top of this file)
 [[ $- != *i* ]] && return
 
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+attach_tmux=$([[ -n "$TMUX" || -n "$NO_TMUX" || "$TERM" = "screen" ]])
+if ! $attach_tmux; then
+  if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+  fi
 fi
 
 # source from Omarchy
@@ -19,6 +22,6 @@ source "$SCRIPT_DIR/shell.sh"
 source "$SCRIPT_DIR/oh-my-zsh"
 
 # Attach to latest tmux session or create new if it don't exist
-if [[ -z "$TMUX" && -z "$NO_TMUX" && "$TERM" != "screen" ]]; then
-  tmux new-session -A -s main
+if $attach_tmux; then
+  tmux new-session -As main
 fi
