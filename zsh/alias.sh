f01636f9459464f409ffc965b9e84d65e8637b8b
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-12 11:09:16 +0545

    feat: ✨ alias to search man pages

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -13,6 +13,7 @@ common_config="--height 40% --layout reverse --tmux"
 alias nvimff="ff --bind 'enter:become(nvim {-1})' $common_config "
 alias gcb="git branch | fzf $common_config --preview 'git show --color=always {-1}' --bind 'enter:become(git checkout {-1})'"
 alias cheat="curl cheat.sh/:list | fzf $common_config --bind 'enter:become(curl cheat.sh/{-1} | less)'"
+alias manman="man -k . | fzf $common_config --bind 'enter:become(man {1})'"
 
 # Tmux
 alias mux="tmuxinator"
