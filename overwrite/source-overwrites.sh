fa0af4a59f9ddb193b6388053834b05b6c155f54
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-21 18:17:10 +0545

    fix: 🐛 move hyprland file correctly

--- overwrite/source-overwrites.sh
+++ overwrite/source-overwrites.sh
@@ -6,7 +6,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../utils/write-to-file.sh"
 
 echo "Setting up Hyprland overrides..."
+write_to_file "$HOME/.config/hypr/hyprland.lua" "require(\"hypr.own_config\")" true
+write_to_file "$HOME/.config/hypr/lua/hypr/own_config.lua" "$(cat "$SCRIPT_DIR/../files_to_copy/hyprland.lua")" true
 write_to_file "$HOME/.config/hypr/hyprlock.conf" "source = $SCRIPT_DIR/hyprlock.overwrite.conf"
+
+echo ""
 
 # Setup bashrc/zsh overrides
 echo "Setting up zsh overrides..."
