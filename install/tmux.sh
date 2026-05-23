185299bc713cc777286ed1c496b6072322c7f60a
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-23 22:02:18 +0545

    refactor: ♻️ don't install tmux plugin manager

--- install/tmux.sh
+++ install/tmux.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-# file inspired from https://github.com/typecraft-dev/omarchy-supplement/blob/main/install-tmux.sh
 
 # Now omarchy has tmux preinstalled
 # if ! command -v tmux &>/dev/null; then
 #   yay -S --noconfirm --needed tmux
 # fi
 
-source "$(dirname "$0")/../utils/clone.sh"
-TPM_DIR="$HOME/.tmux/plugins/tpm"
-
-echo "Installing Tmux Plugin Manager (TPM)..."
-clone_repo https://github.com/tmux-plugins/tpm "$TPM_DIR"
-
-echo "TPM installed successfully!"
-
 # Install tmuxinator
 yay -Sy --noconfirm --needed tmuxinator
-
-# Installing plugins
-~/.tmux/plugins/tpm/bin/install_plugins || true
