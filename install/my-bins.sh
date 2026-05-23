396acb5ac62c7411faf44c72624a6f2f93b81f1e
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-23 22:53:09 +0545

    fix: 🐛 symlink

--- install/my-bins.sh
+++ install/my-bins.sh
@@ -9,7 +9,7 @@ mkdir -p "$DST_BIN"
 
 if [ -d "$SRC_BIN" ] && [ -n "$(ls -A "$SRC_BIN" 2>/dev/null)" ]; then
     chmod +x "$SRC_BIN"/*
-    ln -sf "$SRC_BIN"/* "$DST_BIN/"
+    ln -sfn "$SRC_BIN"/* "$DST_BIN/"
 fi
 
 echo "Bins symlinked to $DST_BIN"
