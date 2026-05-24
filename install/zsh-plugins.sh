964ea929ccd86589308af327a7eb1141887b18fe
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-24 11:19:55 +0545

    refactor: ♻️ simplify code

--- install/zsh-plugins.sh
+++ install/zsh-plugins.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 source "$(dirname "$0")/../utils/clone.sh"
-
-add_plugin() {
-  local repo="$1"
-  [[ "$repo" != */* ]] && repo="zsh-users/$repo"
-  clone_repo "$repo" "$HOME/.zsh/$(basename "$repo")"
-}
-
-add_plugin "zsh-syntax-highlighting"
-add_plugin "zsh-autosuggestions"
+clone_repo zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+clone_repo zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
