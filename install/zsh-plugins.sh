8309a4403828469fcd107a7df0dce920e089738e
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-27 23:43:42 +0545

    fix: 🐛 path while installing zsh plugin

--- install/zsh-plugins.sh
+++ install/zsh-plugins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source "$(dirname "$0")/../utils/clone.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/../utils/clone.sh"
 clone_repo zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 clone_repo zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
