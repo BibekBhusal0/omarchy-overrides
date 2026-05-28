d7f1c3d1f6aaf5fe0a575452b896944163232843
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-28 23:52:38 +0545

    fix: 🐛 zsh autosuggestioni works inside tmux

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -11,5 +11,16 @@ if (( ${+commands[fzf]} ));then
   eval "$(fzf --zsh)"
 fi
 
+# Some settings requied for zsh autosuggestions to work
+export HISTFILE="$HOME/.zsh_history"
+export HISTSIZE=10000
+export SAVEHIST=10000
+export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+setopt APPEND_HISTORY
+setopt SHARE_HISTORY
+setopt INC_APPEND_HISTORY
+setopt EXTENDED_HISTORY
+
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+bindkey '^[[1;5C' forward-word
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
