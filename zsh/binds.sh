ebeebeca1f0de319fbc0d643f9c1009247abe086
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-30 09:47:46 +0545

    feat: ✨ tab to search commands

--- zsh/binds.sh
+++ zsh/binds.sh
@@ -9,3 +9,19 @@ fzf-command-widget() {
 
 zle -N fzf-command-widget
 bindkey '^F' fzf-command-widget
+
+fzf-tab-command-widget() {
+  if [[ -z "$LBUFFER" || "$LBUFFER" == *[[:space:]]* ]]; then
+    zle fzf-completion
+  else
+    local cmd=$(print -rl -- ${(k)commands} ${(k)aliases} | sort -u | fzf --tmux --query="$LBUFFER")
+    if [[ -n "$cmd" ]]; then
+      LBUFFER="$cmd"
+      RBUFFER=""
+    fi
+    zle reset-prompt
+  fi
+}
+
+zle -N fzf-tab-command-widget
+bindkey '^I' fzf-tab-command-widget
