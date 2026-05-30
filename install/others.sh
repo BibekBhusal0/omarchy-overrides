539d88fca3e6f335ae774cb2a8dc249ef2409927
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-30 14:01:21 +0545

    refactor: ♻️ just sorting the installs

--- install/others.sh
+++ install/others.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-omarchy-pkg-aur-add brave-bin
-omarchy-pkg-aur-add yazi
-omarchy-pkg-aur-add legcord-bin
-omarchy-pkg-aur-add hypruler-bin
-omarchy-pkg-aur-add losslesscut-bin
 omarchy-pkg-aur-add bitwarden
 omarchy-pkg-aur-add bitwarden-cli
-omarchy-install-dev-env node
+omarchy-pkg-aur-add brave-bin
+omarchy-pkg-aur-add hypruler-bin
+omarchy-pkg-aur-add legcord-bin
+omarchy-pkg-aur-add losslesscut-bin
+omarchy-pkg-aur-add yazi
+
 omarchy-install-dev-env bun
+omarchy-install-dev-env node
 omarchy-install-dev-env python
 
 npm install -g devmoji
