f6faa91c3b3317d62e997cea8549b69ed1af63a1
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-30 11:05:08 +0545

    feat: ✨ also include functions in fzf command search

--- zsh/binds.sh
+++ zsh/binds.sh
@@ -1,5 +1,5 @@
 fzf-command-widget() {
-  local cmd=$(print -rl -- ${(k)commands} ${(k)aliases} | sort -u | fzf \
+  local cmd=$(print -rl -- ${(k)commands} ${(k)aliases} ${(k)functions} | grep -v '^_' | sort -u | fzf \
     --height=40% --layout=reverse --margin=1 --prompt="")
   if [[ -n $cmd ]]; then
     LBUFFER="$cmd"
@@ -15,7 +15,7 @@ fzf-tab-command-widget() {
   if [[ "$LBUFFER" == *[[:space:]]* ]]; then
     zle fzf-completion
   else
-    local cmd=$(print -rl -- ${(k)commands} ${(k)aliases} | sort -u | fzf \
+    local cmd=$(print -rl -- ${(k)commands} ${(k)aliases} ${(k)functions} | grep -v '^_' | sort -u | fzf \
       --height=40% --layout=reverse --margin=1 --prompt="" \
       --query="$LBUFFER")
     if [[ -n "$cmd" ]]; then
