7610648d8c10aa4b44535b577f381575e712184c
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-30 14:03:01 +0545

    feat: ✨ also helium browser and readest

--- install/others.sh
+++ install/others.sh
@@ -3,9 +3,11 @@
 omarchy-pkg-aur-add bitwarden
 omarchy-pkg-aur-add bitwarden-cli
 omarchy-pkg-aur-add brave-bin
+omarchy-pkg-aur-add helium-browser-bin
 omarchy-pkg-aur-add hypruler-bin
 omarchy-pkg-aur-add legcord-bin
 omarchy-pkg-aur-add losslesscut-bin
+omarchy-pkg-aur-add readest-git
 omarchy-pkg-aur-add yazi
 
 omarchy-install-dev-env bun
