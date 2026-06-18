a2b8d6fc1098fd23899019e34828e7f80249d1a4
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-06-18 23:57:47 +0545

    fix: install order

--- install/others.sh
+++ install/others.sh
@@ -7,14 +7,16 @@ omarchy-pkg-aur-add helium-browser-bin
 omarchy-pkg-aur-add hypruler-bin
 omarchy-pkg-aur-add legcord-bin
 omarchy-pkg-aur-add losslesscut-bin
-omarchy-pkg-aur-add readest-git
 omarchy-pkg-aur-add yazi
 
 omarchy-install-dev-env bun
 omarchy-install-dev-env node
 omarchy-install-dev-env python
 
+omarchy-pkg-aur-add readest-git
+
 npm install -g devmoji
 npm install -g yarn
 
+bun add -g opencode-ai
 curl -sSL https://usegitai.com/install.sh | bash
