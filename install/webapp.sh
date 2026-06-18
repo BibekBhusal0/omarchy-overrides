03b35486aa617f79969cbefcb5e76c15c6420ef8
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-06-18 23:27:41 +0545

    fix installing webapps

--- install/webapp.sh
+++ install/webapp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 omarchy-webapp-install "Chess" https://chess.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chess.png
-omarchy-webapp-install "Gemini (AI Studio)" https://aistudio.google.com/prompts/new_chat?model=gemini-3-pro-preview https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/gemini.png
+omarchy-webapp-install "Gemini (AI Studio)" https://aistudio.google.com/prompts/new_chat?model=gemini-3.1-pro-preview https://cdn.jsdelivr.net/npm/@lobehub/icons-static-png@latest/light/gemini-color.png
 omarchy-webapp-install "Podcast" https://pocketcasts.com/podcasts https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/pocket-casts.png
-omarchy-webapp-install "Gemini (AI Studio)" https://aistudio.google.com/prompts/new_chat?model=gemini-3-pro-preview https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/gemini.png
 omarchy-webapp-install "Reddit" https://reddit.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/reddit.png
