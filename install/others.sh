9e8d1612425958bda4454e7824fc91f3630b7515
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-06-26 21:36:55 +0545

    feat: ✨ use bun to install devmoji and yarn

--- install/others.sh
+++ install/others.sh
@@ -9,15 +9,15 @@ omarchy-pkg-aur-add vesktop-bin
 omarchy-pkg-aur-add losslesscut-bin
 omarchy-pkg-aur-add yazi
 
-omarchy-install-dev-env bun
 omarchy-install-dev-env node
+omarchy-install-dev-env bun
 omarchy-install-dev-env python
 omarchy-install-terminal ghostty
 omarchy-default-terminal ghostty
 
 omarchy-pkg-aur-add readest-git
 
-npm install -g devmoji
-npm install -g yarn
+bun install -g devmoji
+bun install -g yarn
 
 curl -sSL https://usegitai.com/install.sh | bash
