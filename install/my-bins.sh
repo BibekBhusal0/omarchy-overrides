75e4ae8b831d9201d7241be016b69c9395bc379c
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-14 20:36:19 +0545

    feat: ✨ keymaps and waker picker for focusd

--- install/my-bins.sh
+++ install/my-bins.sh
@@ -5,5 +5,6 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 source "$REPO_ROOT/utils/symlink.sh"
 source "$REPO_ROOT/utils/write-to-file.sh"
 create_symlink "$REPO_ROOT/bin" "$HOME/.local/my-bins"
+chmod +x "$REPO_ROOT/bin"/*
 
 write_to_file "$HOME/.config/uwsm/env" "PATH=\$HOME/.local/my-bins:\$PATH"
