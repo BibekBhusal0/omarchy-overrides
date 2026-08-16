e2fcde554f338af1c8b534f7a3e86ccdbbfb1f28
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-08-16 15:16:55 +0545

    feat: ✨ insall extensions/theme for spicetify

--- install/spicetify.sh
+++ install/spicetify.sh
@@ -36,4 +36,31 @@ if [[ ! -f "$HOME/.config/spotify/prefs" ]]; then
   exit 0
 fi
 
-spicetify backup apply
+curl -fsSL https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh | sh
+
+THEME_DIR="$HOME/.config/spicetify/Themes/Ziro"
+if [[ ! -f "$THEME_DIR/color.ini" || ! -f "$THEME_DIR/user.css" ]]; then
+  mkdir -p "$THEME_DIR"
+  curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-themes/master/Ziro/color.ini -o "$THEME_DIR/color.ini"
+  curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-themes/master/Ziro/user.css -o "$THEME_DIR/user.css"
+fi
+spicetify config current_theme Ziro color_scheme green-dark
+
+EXT_FILE="$HOME/.config/spicetify/Extensions/adblock.js"
+if [[ ! -f "$EXT_FILE" ]]; then
+  curl -fsSL https://raw.githubusercontent.com/rxri/spicetify-extensions/main/adblock/adblock.js -o "$EXT_FILE"
+fi
+EXTENSIONS=(shuffle+.js keyboardShortcut.js adblock.js)
+
+enabled=$(spicetify config extensions | tr '\n' ' ')
+for ext in "${EXTENSIONS[@]}"; do
+  case "|$enabled|" in *"|$ext|"*) ;; *) spicetify config extensions "$ext" ;; esac
+done
+
+if ! grep -q '^\[Backup\]' "$(spicetify -c)"; then
+  spicetify backup
+fi
+spicetify apply
+
+echo "Theme: $(spicetify config current_theme)/$(spicetify config color_scheme)"
+echo "Extensions: $(spicetify config extensions | tr '\n' ' ')"
