15527c3899faa46bc420978350dde2a4a0a6fde5
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-08-16 19:25:12 +0545

    fix: 🐛 lofi daemon start correclty

--- install/lofi-music.sh
+++ install/lofi-music.sh
@@ -10,18 +10,3 @@ fi
 
 
 omarchy-tui-install "Lofi Music" "cliamp --shuffle --auto-play $HOME/Music/lofimusic/" tile "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu1U7EHisLuM3va3QRXNBmnSiSHFc1QnidSYaeoiv9qz-LZkn6KYlysHQ&s=10"
-
-source "$(dirname "${BASH_SOURCE[0]}")/../utils/write-to-file.sh"
-write_to_file "$HOME/.config/systemd/user/cliamp.service" "$(cat <<'EOF'
-[Unit]
-Description=cliamp headless music player
-
-[Service]
-ExecStart=/usr/bin/cliamp --daemon --shuffle --auto-play $HOME/Music/lofimusic/
-Restart=on-failure
-
-[Install]
-WantedBy=default.target
-EOF
-)" true
-systemctl --user enable cliamp
