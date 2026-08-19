25878874f6443cbef78add77f4af0c5379b42487
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-08-19 11:43:47 +0545

    feat: ✨ install omarchy plugins properly

--- config/omarchy.sh
+++ config/omarchy.sh
@@ -7,6 +7,7 @@ omarchy-plugin-disable omarchy.dev-gallery
 omarchy-plugin-enable omarchy.tailscale
 
 source "$(dirname "${BASH_SOURCE[0]}")/../utils/clone.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/../utils/symlink.sh"
 
 # Adding my own plugins
 clone omarchy-shell-plugin ~/Code/omarchy-shell-plugin/
@@ -16,14 +17,32 @@ cp -r ~/Code/omarchy-shell-plugin/focusd ~/.config/omarchy/plugins/focusd
 cp -r ~/Code/omarchy-shell-plugin/obsidian-search ~/.config/omarchy/plugins/obsidian-search
 cp -r ~/Code/omarchy-shell-plugin/readest ~/.config/omarchy/plugins/readest
 cp -r ~/Code/omarchy-shell-plugin/media ~/.config/omarchy/plugins/media
+
+# some other plugins I love
+clone markbus-ai/omarchy-opencode-usage ~/.config/omarchy/plugins/opencode --depth=1
+clone Praveensenpai/omarchy-refined-menu ~/Code/random/omarchy-refined-menu --depth=1
+create_symlink ~/Code/random/omarchy-refined-menu/plugin ~/.config/omarchy/plugins/menu
+
+omarchy-shell shell rescanPlugins
 omarchy plugin validate ~/.config/omarchy/plugins/focusd
 omarchy plugin validate ~/.config/omarchy/plugins/obsidian-search
 omarchy plugin validate ~/.config/omarchy/plugins/readest
 omarchy plugin validate ~/.config/omarchy/plugins/media
-omarchy-shell shell rescanPlugins
+omarchy plugin validate ~/.config/omarchy/plugins/opencode
+omarchy plugin validate ~/.config/omarchy/plugins/menu
+
+omarchy plugin enable bibek.media --section left
 omarchy plugin enable bibek.focusd --section left
 omarchy plugin enable bibek.obsidian-search
 omarchy plugin enable bibek.readest
-omarchy plugin enable bibek.media --section left
+omarchy plugin enable markbusai.opencode-usage --section right
+omarchy plugin enable paisen.menu --section left
+
+# Remove menu from bar but keep it as a plugin
+jq '
+  .bar.layout.left |= map(select(.id != "paisen.menu")) |
+  .plugins += [{"id": "paisen.menu"}]
+' ~/.config/omarchy/shell.json > /tmp/shell.json 
+mv /tmp/shell.json ~/.config/omarchy/shell.json
 
 omarchy-restart-shell
